c57f713ce61a34d70f444b1b8ef020f62ddad653
RasiShruthisuresh <71762234045@cit.edu.in>
2026-06-19 12:04:35 +0530

    Shopping cart game

---
+++ src/ShoppingCartGame.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class ShoppingCartGame {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        String item;
+        double price;
+        int quantity;
+        char currency = '$';
+        double total;
+        System.out.println("What item would you like to buy?");
+        item = scanner.nextLine();
+        System.out.print("What is the price for each?: " );
+        price= scanner.nextDouble();
+        System.out.print("How many would you like ?");
+        quantity=scanner.nextInt();
+        total = price * quantity;
+        System.out.print("TOTAL: " +total);
+        scanner.close();
+    }
+}
